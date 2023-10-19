c3fe9360f65acb0d1a626dbe377b777905e8385c
bharateesh <bharateeshwork@gmail.com>
2023-10-19 14:34:53 +0530

    Stereotype annotations: @Component and @Value full upload

---
+++ springcore/src/main/java/com/springcore/stereotype/Student.java
@@ -0,0 +1,40 @@
+package com.springcore.stereotype;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component("ob")
+public class Student {
+	@Value("bharateesh")
+	private String studentName;
+	@Value("tumkur")
+	private String city;
+	@Value("#{list}")
+	private List<String> address;
+	public String getStudentName() {
+		return studentName;
+	}
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	@Override
+	public String toString() {
+		return "Student [studentName=" + studentName + ", city=" + city + ", address=" + address + "]";
+	}
+	public List<String> getAddress() {
+		return address;
+	}
+	public void setAddress(List<String> address) {
+		this.address = address;
+	}
+	
+	
+}
